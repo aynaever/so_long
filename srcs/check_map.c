e71845331c3df5bb03fa78b98ed0681967587056
Aymane Naouadir <aymane.naouadir18@gmail.com>
2021-10-19 15:10:43 +0100

    check map works in all cases

--- srcs/check_map.c
+++ srcs/check_map.c
@@ -6,7 +6,7 @@
 /*   By: anaouadi <anaouadi@student.42wolfsbu       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 10:14:29 by anaouadi          #+#    #+#             */
-/*   Updated: 2021/10/19 14:09:55 by anaouadi         ###   ########.fr       */
+/*   Updated: 2021/10/19 15:08:47 by anaouadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,30 +55,43 @@ static	int	check_valid_chars(char	*line)
 	return (1);
 }
 
-static int	count_comps(char *line, t_ncomps comps)
+static int	count_comps(char *line, int	*ex, int *col, int *st)
 {
 	int	i;
 
 	i = 0;
-	comps.n_exits = 0;
-	comps.n_collects = 0;
-	comps.n_starts = 0;
 	while (i < ft_strlen(line) - 2)
 	{
 		if (line[i] == 'E')
-			comps.n_exits++;
-		else if(line[i] == 'C')
-			comps.n_collects++;
-		else if(line[i] == 'P')
-			comps.n_starts++;
+			(*ex)++;
+		else if (line[i] == 'C')
+			(*col)++;
+		else if (line[i] == 'P')
+			(*st)++;
 		i++;
 	}
 	return (1);
 }
 
-static	int	check_comps(t_ncomps comps)
+static	int	check_comps(char	*path)
 {
-	if (comps.n_exits >= 1 && comps.n_collects >= 1 && comps.n_starts >= 1)
+	char		*line;
+	int			fd;
+	int			ex;
+	int			col;
+	int			st;
+
+	fd = open(path, O_RDONLY);
+	line = get_next_line(fd);
+	ex = 0;
+	col = 0;
+	st = 0;
+	while (line != NULL)
+	{
+		count_comps(line, &ex, &col, &st);
+		line = get_next_line(fd);
+	}
+	if (ex >= 1 && col >= 1 && st >= 1)
 		return (1);
 	return (0);
 }
@@ -88,7 +101,7 @@ int	check_map(int argc, char	*path)
 	int		fd;
 	size_t	nl;
 	char	*line;
-	size_t	len_frst_line;
+	size_t	len_frst;
 	char	*old_line;
 
 	fd = open(path, O_RDONLY);
@@ -96,17 +109,18 @@ int	check_map(int argc, char	*path)
 	if (fd == -1 || argc != 2)
 		return (0);
 	line = get_next_line(fd);
-	len_frst_line = ft_strlen(line);
+	len_frst = ft_strlen(line);
 	while (line != NULL)
 	{
 		old_line = line;
 		if (check_valid_chars(line) == 0 || check_line(nl, line) == 0
-			|| ft_strlen(line) != len_frst_line)
+			|| ft_strlen(line) != len_frst)
 			return (0);
 		line = get_next_line(fd);
 		nl++;
 	}
-	if (nl == len_frst_line || check_line(1, old_line) == 0)
+	if (nl == len_frst || check_line(1, old_line) == 0
+		|| check_comps(path) == 0)
 		return (0);
 	return (1);
 }
--- srcs/so_long.h
+++ srcs/so_long.h
@@ -6,18 +6,12 @@
 /*   By: anaouadi <anaouadi@student.42wolfsbu       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 10:21:59 by anaouadi          #+#    #+#             */
-/*   Updated: 2021/10/19 13:34:58 by anaouadi         ###   ########.fr       */
+/*   Updated: 2021/10/19 14:44:51 by anaouadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	SO_LONG_H
 # define	SO_LONG_H
-
-typedef	struct	s_ncomps {
-	int	n_exits;
-	int	n_collects;
-	int	n_starts;
-}				t_ncomps;
 
 # include "../minilibx-linux/mlx.h"
 # include "../minilibx-linux/mlx_int.h"
