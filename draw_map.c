1f9c14b524f6d9f0cf1d31e99034cb4a24d36768
Aymane Naouadir <aymane.naouadir18@gmail.com>
2021-10-22 20:22:07 +0100

    fixing main.c and draw_map.c

--- draw_map.c
+++ draw_map.c
@@ -6,7 +6,7 @@
 /*   By: anaouadi <anaouadi@student.42wolfsbu       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 10:21:55 by anaouadi          #+#    #+#             */
-/*   Updated: 2021/10/21 13:03:18 by anaouadi         ###   ########.fr       */
+/*   Updated: 2021/10/22 20:20:41 by anaouadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ int	draw_map(void)
 	void	*mlx;
 	void	*win;
 	void	*img;
-	int		*width = 0;
-	int		*height = 0;
+	int		width;
+	int		height;
 
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 500, 500, "Funny Game");
-	img = mlx_xpm_file_to_image(mlx, "exit.xpm", width, height);
+	img = mlx_xpm_file_to_image(mlx, "img/exit.xpm", &width, &height);
 	printf("img = %p\n", img);
-	mlx_loop(mlx);
 	mlx_put_image_to_window(mlx, win, img, 0, 0);
+	mlx_loop(mlx);
 	return (0);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: anaouadi <anaouadi@student.42wolfsbu       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 16:37:33 by anaouadi          #+#    #+#             */
-/*   Updated: 2021/10/22 19:41:18 by anaouadi         ###   ########.fr       */
+/*   Updated: 2021/10/22 20:21:31 by anaouadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,6 @@ int	main(int argc, char	**argv)
 		return (0);
 	}
 	store_map(argv[1]);
-	//draw_map(argv[1]);
-	void	*mlx;
-	void	*win;
-	int		height, width;
-	void	*img;
-
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 500,500, "fly");
-	img = mlx_xpm_file_to_image(mlx, "/home/me/Projects/level_2/so_long/img/exit.xpm", &width, &height);
-	printf("img_ptr = %p\n", img);
-	mlx_put_image_to_window(mlx, win, img, 0, 0);
-	mlx_loop(mlx);
+	draw_map(argv[1]);
 	return (0);
 }
